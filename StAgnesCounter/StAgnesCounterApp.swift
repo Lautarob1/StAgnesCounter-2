9681df2b1efaef7da598f17157a6844f1bd974c2
LBarrera <lautarobarrerab@gmail.com>
2024-04-20 13:57:16 -0400

    Initial Commit

---
+++ StAgnesCounter/StAgnesCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StAgnesCounterApp.swift
+//  StAgnesCounter
+//
+//  Created by EFI-Admin on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct StAgnesCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
